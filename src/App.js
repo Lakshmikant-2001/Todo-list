6b3fbbf6c4dce498374eb48d5be281a694830308
Lakshmikant.K <l.kumar@saama.com>
2022-05-19 19:33:32 +0530

    add TodoForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //components
 import Navbar from "./components/Header";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <TodoForm />
+      </main>
     </div>
   );
 }
